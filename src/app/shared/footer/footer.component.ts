16212cd6061e70f172e12ebd81183782e110e4ce
AbdulrahmanElshaphei <abdulrahmanelshaphei@gmail.com>
2026-03-07 17:22:58 +0200

    third

--- src/app/shared/footer/footer.component.ts
+++ src/app/shared/footer/footer.component.ts
@@ -4,12 +4,20 @@ import { RouterLink, RouterLinkActive } from '@angular/router';
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [RouterLink, RouterLinkActive],
+  imports: [RouterLink],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.css'
 })
 export class FooterComponent {
-  // openFacebook() {
-  //   window.open('https://web.facebook.com/mimmisa.mohsen', '_blank');
-  // }
+  openInsta() {
+    window.open('https://www.instagram.com/code.cast6/', '_blank');
+  }
+
+  openFace(){
+    window.open('https://web.facebook.com/profile.php?id=61585531988084', '_blank');
+  }
+
+  openTik(){
+    window.open('https://www.tiktok.com/@code.cast6?lang=en-GB', '_blank');
+  }
 }
